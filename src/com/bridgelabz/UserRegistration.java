9aa81168af68fee35e8b6e33115d85637e6dd047
[Digvikadam] <[digvikadam168@gmail.com]>
2021-11-21 11:31:03 +0530

    Added: Validation Of Emails by using all Email Samples

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -1,13 +1,20 @@
 package com.bridgelabz;
 
+import java.util.ArrayList;
 public class UserRegistration {
 	public static void main(String[] args) {
+		ArrayList<String> email = new ArrayList<>();
+		UserInputOfEmails emails = new UserInputOfEmails();
 		ValidationOfUserRegistration validationOf = new ValidationOfUserRegistration();
-//	        validationOf.validateFirstName("Digvijay");
-//	        validationOf.validateLastName("Kadam");
-//			validationOf.validateEmailId("digvijaykadam168@gmail.com");
-//	        validationOf.validateMobileNumber("91 9876543210");
-//	        validationOf.validatePasswordRuleOne("Digvijay68@)+");
-			System.out.println(validationOf.validatePasswordRuleFour("Digvijay@643nhhjj21hg"));
-	  }
+//      validationOf.validateFirstName("Digvijay");
+//      validationOf.validateLastName("Kadam");
+//		validationOf.validateEmailId("digvijaykadam168@gmail.com");
+//      validationOf.validateMobileNumber("91 9876543210");
+//      validationOf.validatePasswordRuleOne("Digvijay68@)+");
+		System.out.println(validationOf.validatePasswordRuleFour("Digvijay@643nhhjj21hg"));
+		email = emails.userDataOfEmails();
+		for(String e : email){
+			validationOf.validateEmailId(e);
+		}
+	}
 }
