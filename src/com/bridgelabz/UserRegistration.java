35e926601ea67b10c0fc6b2bda1640c37f1bf8d2
[Digvikadam] <[digvikadam168@gmail.com]>
2021-11-21 11:10:30 +0530

    Added: Validation Of Password by rule with One Character UpperCase

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -1,12 +1,13 @@
 package com.bridgelabz;
 
 public class UserRegistration {
-	  public static void main(String[] args) {
-		  ValidationOfUserRegistration validationOf = new ValidationOfUserRegistration();
-	        validationOf.validateFirstName("Digvijay");
-	        validationOf.validateLastName("Kadam");
-	        validationOf.validateEmailId("digvijaykadam168@gmail.com");
-	        validationOf.validateMobileNumber("91 9876543210");
-	        validationOf.validatePasswordRuleOne("Digvijay68@)+");
+	public static void main(String[] args) {
+		ValidationOfUserRegistration validationOf = new ValidationOfUserRegistration();
+//	        validationOf.validateFirstName("Digvijay");
+//	        validationOf.validateLastName("Kadam");
+//			validationOf.validateEmailId("digvijaykadam168@gmail.com");
+//	        validationOf.validateMobileNumber("91 9876543210");
+//	        validationOf.validatePasswordRuleOne("Digvijay68@)+");
+	        System.out.println(validationOf.validatePasswordRuleTwo("DigVijay@16+"));
 	  }
 }
