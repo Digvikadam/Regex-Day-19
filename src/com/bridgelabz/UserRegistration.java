b95262f477f833abe15a5747083b7905435df169
[Digvikadam] <[digvikadam168@gmail.com]>
2021-11-21 10:56:00 +0530

    Added: Validation Of Mobile Number

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -3,8 +3,9 @@ package com.bridgelabz;
 public class UserRegistration {
 	  public static void main(String[] args) {
 		  ValidationOfUserRegistration validationOf = new ValidationOfUserRegistration();
-	        validationOf.validateFirstName();
-	        validationOf.validateLastName();
-	        validationOf.validateEmailId();
+	        validationOf.validateFirstName("Digvijay");
+	        validationOf.validateLastName("Kadam");
+	        validationOf.validateEmailId("digvijaykadam168@gmail.com");
+	        validationOf.validateMobileNumber("91 9876543210");
 	  }
 }
